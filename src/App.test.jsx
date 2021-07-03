149a2435dde7da6258e40b9244752c2655db56f8
hanvit7 <hanvit7@gmail.com>
2021-07-03 12:06:03 +0900

    [INITIAL] 프로젝트 기본 세팅
    codesoom/react-week8-assignment-1에 기반으로 redux와 @emotion 들어냄.
    * redux부분은 추후 처음부터 세팅해볼 생각으로 제외시킴.
    * @emotion은 시간상 기존에 익숙한 material-ui를 사용할 예정으로 제외시킴.

---
+++ src/App.test.jsx
@@ -0,0 +1,31 @@
+import { MemoryRouter } from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  function renderApp({ path }) {
+    return render(
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>,
+    );
+  }
+
+  context('with path /', () => {
+    it('renders the home page', () => {
+      const { container } = renderApp({ path: '/' });
+
+      expect(container).toHaveTextContent('Home');
+    });
+  });
+
+  context('with invalid path', () => {
+    it('renders the not found page', () => {
+      const { container } = renderApp({ path: '/xxx' });
+
+      expect(container).toHaveTextContent('Not Found');
+    });
+  });
+});
